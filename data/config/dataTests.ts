a7573689e05e53803f0b2f8a05bdb52f7b9872a3
fainnerramirez <fainnerramirez@gmail.com>
2025-08-12 13:49:53 -0500

    Test buscar vuelos | Bogotá a medellín | agosto 28 a agosto 29 | solo ida

--- data/config/dataTests.ts
+++ data/config/dataTests.ts
@@ -6,8 +6,6 @@ const tests: TGenericCopys[] = [
         description: "ruta de barranquilla a bogotá",
         homeCiudadOrigen: "BOG",
         homeCiudadDestino: "MDE",
-        homeisActiveOptionOutbound: true,
-        homePassengerAdults: 4,
         targetPage: "home"
     }
     // {
