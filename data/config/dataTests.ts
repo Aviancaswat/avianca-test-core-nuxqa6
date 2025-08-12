762506f2968acd3499248ed0108bd7f0c9d94981
fainnerramirez <fainnerramirez@gmail.com>
2025-08-12 17:48:43 -0500

    prueba id - Test bogotá a medellín - ida y vuelta - agosto 28 a agosto 29

--- data/config/dataTests.ts
+++ data/config/dataTests.ts
@@ -2,22 +2,19 @@ import { TGenericCopys } from "../copys";
 
 const tests: TGenericCopys[] = [
     {
-        id: "Prueba de ruta de BAQ-MDE",
+        id: "ID PRUEBA - Test bogotá a medellín - ida y vuelta - agosto 28 a agosto 29",
         description: "ruta de barranquilla a bogotá",
         homeCiudadOrigen: "BOG",
         homeCiudadDestino: "MDE",
         targetPage: "home"
+    },
+    {
+        id: "OTRO ID PRUEBA - selección de tarifa economy | Bogotá a barranquilla | agosto 28 a agosto 29 | solo ida",
+        description: "ruta de barranquilla a bogotá",
+        homeCiudadOrigen: "BOG",
+        homeCiudadDestino: "BAQ",
+        targetPage: "passenger",
     }
-    // {
-    //     id: "Test selección de tarifa economy | Bogotá a barranquilla | agosto 28 a agosto 29 | solo ida",
-    //     description: "ruta de barranquilla a bogotá",
-    //     homeCiudadOrigen: "BOG",
-    //     homeCiudadDestino: "BAQ",
-    //     homeisActiveOptionOutbound: true,
-    //     homePassengerAdults: 2,
-    //     targetPage: "passenger",
-    //     seatTarifaDeAsientos: "economy"
-    // }
 ]
 
 export { tests };
