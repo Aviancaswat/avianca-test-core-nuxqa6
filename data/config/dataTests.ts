437359ca1c336c594c92059907c2e8bf0c962d8f
fainnerramirez <fainnerramirez@gmail.com>
2025-08-12 13:30:26 -0500

    Test buscar vuelos | Bogotá a medellín | agosto 28 a agosto 29 | solo ida

--- data/config/dataTests.ts
+++ data/config/dataTests.ts
@@ -2,7 +2,7 @@ import { TGenericCopys } from "../copys";
 
 const tests: TGenericCopys[] = [
     {
-        id: "id unico",
+        id: "Test buscar vuelos | Bogotá a medellín | agosto 28 a agosto 29 | solo ida",
         description: "ruta de barranquilla a bogotá",
         homeCiudadOrigen: "BOG",
         homeCiudadDestino: "MDE",
@@ -10,6 +10,16 @@ const tests: TGenericCopys[] = [
         homePassengerAdults: 4,
         targetPage: "home"
     }
+    // {
+    //     id: "Test selección de tarifa economy | Bogotá a barranquilla | agosto 28 a agosto 29 | solo ida",
+    //     description: "ruta de barranquilla a bogotá",
+    //     homeCiudadOrigen: "BOG",
+    //     homeCiudadDestino: "BAQ",
+    //     homeisActiveOptionOutbound: true,
+    //     homePassengerAdults: 2,
+    //     targetPage: "passenger",
+    //     seatTarifaDeAsientos: "economy"
+    // }
 ]
 
 export { tests };
