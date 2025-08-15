794379f82b22d1e3a9af8e7c630eb0468806dfa6
fainnerramirez <fainnerramirez@gmail.com>
2025-08-15 15:28:41 -0500

    feat: mejpras en flujo booking

--- data/config/dataTests.ts
+++ data/config/dataTests.ts
@@ -6,8 +6,7 @@ const tests: TGenericCopys[] = [
         description: "ruta de barranquilla a bogotá",
         homeCiudadOrigen: "BOG",
         homeCiudadDestino: "MDE",
-        homeisActiveOptionOutbound: true,
-        targetPage: "passenger"
+        targetPage: "home"
     }
 ]
 
